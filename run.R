34b5fff52b1fce65eb57e8168946b22e7266abca
Katharina Kaelin <katharina.kaelin@statistik.ji.zh.ch>
2026-08-28 20:44:28 +0200

    add run

---
+++ run.R
@@ -0,0 +1,14 @@
+source("./R/setup.R")
+
+# logger ----
+#source("./R/create-csv.R")
+#source("./R/danfoss-logger.R")
+
+# auswertung -----
+source("./R/import.R")
+source("./R/danfoss_auswertung.R")
+source("./R/danfoss_auswertung_liter.R")
+
+# ?
+source("./R/local_information.R")
+
